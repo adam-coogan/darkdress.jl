2a0e64c306aac2e83eb7734df91acc28f694b19a
Adam Coogan <dr.adam.coogan@gmail.com>
2021-03-16 23:12:29 +0100

    update SNR, add loglike

--- analysis.jl
+++ analysis.jl
@@ -70,10 +70,80 @@ function fim_errs(fₗ, fₕ, f_c, system::T) where T <: Binary
     return sqrt.([Σ[i, i] for i in 1:size(Σ)[1]])
 end
 
-"""
-Signal-to-noise ratio.
-"""
-function snr(fₗ, fₕ, system::T) where T <: Binary
-    integrand(f) = amp₊(f, system)^2 / Sₙ_LISA(f)
-    return √(4 * quadgk(integrand, fₗ, fₕ, rtol=1e-7)[1])
+using FastGaussQuadrature
+using Trapz
+using LinearAlgebra
+
+# These take fc so they can work with VacuumBinary and StaticDress as well as
+# DynamicDress.
+
+function calculate_SNR(system, fₗ, fₕ, fc; N_nodes=10000, USE_GAUSS=true)
+    fh = min(fₕ, fc)
+
+    modh_integrand(f) = 4 * amp₊(f, system)^2 / Sₙ_LISA(f)
+
+    #Use fixed Gauss quadrature, or Trapz
+    if USE_GAUSS
+        nodes, weights = gausslegendre(N_nodes)
+        f̄ = (fh + fₗ)/2
+        Δf = (fh - fₗ)
+        fs = f̄ .+ nodes * Δf / 2
+        modh = (Δf / 2 * dot(weights, modh_integrand.(fs)))
+    else  # trapz
+        fs = range(fₗ, fh, length=N_nodes)
+        modh = trapz(fs, modh_integrand.(fs))
+    end
+
+    return sqrt(modh)
+end
+
+function calculate_match_unnorm(system_1, system_2, fₗ, fₕ, fc_1, fc_2; N_nodes, USE_GAUSS)
+    # Cut integral off at ISCO of first system to merge
+    fh = min(fₕ, fc_1, fc_2)
+
+    f̄ = (fh + fₗ)/2
+    Δf = (fh - fₗ)
+
+    # TODO: should it be fc_1 or fc_2 here?
+    calcΔΨ(f) = Ψ(f, fc_1, system_1) - Ψ(f, fc_2, system_2)
+    calcAmp(f) = 4 * amp₊(f, system_1) * amp₊(f, system_2) / Sₙ_LISA(f)
+
+    if USE_GAUSS
+        nodes, weights = gausslegendre(N_nodes)
+        f_vals = f̄ .+ nodes * Δf / 2
+    else
+        f_vals = range(fₗ, fh, length=N_nodes)
+    end
+
+    ΔΨ = calcΔΨ.(f_vals)
+    Amp = calcAmp.(f_vals)
+    integs_re = cos.(ΔΨ) .* Amp
+    integs_im = sin.(ΔΨ) .* Amp
+
+    if USE_GAUSS
+        x_re = (Δf/2 * dot(weights,  integs_re))
+        x_im = (Δf/2 * dot(weights,  integs_im))
+    else
+        x_re = trapz(f_vals, integs_re)
+        x_im = trapz(f_vals, integs_im)
+    end
+
+    return x_re, x_im
+end
+
+function calculate_loglike(
+    system_h, system_d, fₗ, fₕ, fc_h, fc_d;
+    optimize_dₗ_ι=true, optimize_Φ_c=true, N_nodes=1000, USE_GAUSS=true
+)
+    # Calculate real and imaginary parts of the overlap <d|h>
+    dh_re, dh_im = calculate_match_unnorm(system_d, system_h, fₗ, fₕ, fc_h, fc_d; N_nodes, USE_GAUSS)
+
+    # Calculate the overlap <h|h>
+    hh = calculate_SNR(system_h, fₗ, fₕ, fc_h; N_nodes, USE_GAUSS)^2
+    
+    # Optimise over phase at coalescence and dₗ_ι
+    dh = optimize_Φ_c ? sqrt(dh_re^2 + dh_im^2) : dh_re
+    r = optimize_dₗ_ι ? dh / hh : 1.
+
+    return r * dh - 1/2 * r^2 * hh    
 end
